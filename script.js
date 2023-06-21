568fc364961fc96c421794ece15fa35559902f9f
sandajama <sandajama29@gmail.com>
2023-06-21 15:29:15 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Good Luck 💪🏾
+function plusHashCount(str){
+    let plus=0
+    let hash=0
+    let makearray=str.split("")
+    let newarray=[]
+
+    for(let i=0; i<makearray.length;i++){
+        if(str[i]=="#"){
+            hash ++
+        }else if(str[i]=="+"){
+            plus ++
+        }
+    }
+    newarray.push(hash)
+    newarray.push(plus)
+    console.log(newarray);
+
+    return makearray
+}
+plusHashCount("###+") 
+plusHashCount("##+++#") 
+plusHashCount("#+++#+#++#") 
+plusHashCount("") 
